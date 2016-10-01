84f0146186840f0ff3649bed3ad050b8f2cb398b
Jenny Wong <jew007@ucsd.edu>
2016-10-01 13:41:04 -0700

    bare bonez

--- venmo_analysis.py
+++ venmo_analysis.py
@@ -3,15 +3,29 @@ import csv
 
 def readVenmo():
 
+    result = []
     desc = []
     date = []
     with open('venmo_data.tsv', 'r') as tsv:
         for line in csv.reader(tsv, dialect="excel-tab"):
             desc.append(unicode(line[0], 'utf-8'))
             date.append(line[1])
-    print 'file read'
 
-    print desc
-    print date
+    for d in desc:
+        dnew = d.encode('unicode_escape')
+        r = analyze(dnew)
+        if r != None:
+            result.append(r)
+
+    printReciept(result)
+
+def analyze(d):
+    if '\U0001f32e' in d:
+        return 'You love tacos.'
+
+def printReciept(result):
+    print 'According to our analysis...'
+    for l in result:
+        print l
 
 readVenmo()
